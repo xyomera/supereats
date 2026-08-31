857a57c8be2a0bef731c29e71090ada2141a771c
xyomera <2553370597@qq.com>
2026-08-31 09:02:44 +0800

    feat(mod): add super apple item

---
+++ src/main/java/com/xyomera/mixin/ExampleMixin.java
@@ -0,0 +1,21 @@
+/*
+	Copyright (C) 2026 xyomera,
+	All rights reserved.
+	but based on the Apache 2.0 license.
+ */
+
+package com.xyomera.mixin;
+
+import net.minecraft.server.MinecraftServer;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(MinecraftServer.class)
+public class ExampleMixin {
+	@Inject(at = @At("HEAD"), method = "loadLevel")
+	private void init(CallbackInfo info) {
+		// This code is injected into the start of MinecraftServer.loadLevel()V
+	}
+}
